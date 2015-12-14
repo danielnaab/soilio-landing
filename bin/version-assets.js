17f48b5a02980d8106a9eba20b54426857241b4e
Daniel Naab <danielnaab@gmail.com>
2015-12-14 13:26:44 -0600

    Speed optimizations: inline css and javascript; gzip assets for s3 hosting.

--- bin/version-assets.js
+++ bin/version-assets.js
@@ -6,5 +6,5 @@ var Version = require('node-version-assets')
 new Version({
     //assets: ['dist/styles.css', 'dist/images/background.jpg', 'dist/bundle.js'],
     assets: ['dist/styles.css'],
-    grepFiles: ['dist/index.html']
+    grepFiles: ['dist/index-src.html']
 }).run()
